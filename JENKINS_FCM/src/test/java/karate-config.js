a5a916f432a6f33fb3c3807102153c76320443e9
swathi <swathi@GISI-0NVH5P.gisi.com>
2019-08-21 18:38:04 +0530

    third commit

---
+++ JENKINS_FCM/src/test/java/karate-config.js
@@ -0,0 +1,24 @@
+function fn() {    
+  var env = karate.env; // get system property 'karate.env'
+  karate.log('karate.env system property was:', env);
+  if (!env) {
+    env = 'dev';
+  }
+  var config = {
+    env: env,
+    baseURL: 'http://localhost/quote/api/Quotes',
+    database:"test222",
+    colname:"user222"
+
+  }
+  if (env == 'dev') {
+    // customize
+    // e.g. config.foo = 'bar';
+  } else if (env == 'e2e') {
+    // customize
+  }
+  karate.configure('connectTimeout', 80000);
+  karate.configure('readTimeout', 80000);
+ 
+  return config;
+}
