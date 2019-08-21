8c29760a2615b57e02a4babc8ca4e3dafc7e01a8
swathi <swathi@GISI-0NVH5P.gisi.com>
2019-08-21 15:58:06 +0530

    json report

--- JENKINS_FCM/src/test/java/quotes/quoteRunner.java
+++ JENKINS_FCM/src/test/java/quotes/quoteRunner.java
@@ -1,11 +1,14 @@
 package quotes;
 import com.intuit.karate.junit4.Karate;
 
+import cucumber.api.CucumberOptions;
 
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
-
+@CucumberOptions(features="quote.feature",
+plugin= {"json:target/cucumber.json"}
+		)
 public class quoteRunner {
 
 }
